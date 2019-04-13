e855c9cb7c613f55215bc532006f426e81b9d682
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-04-13 18:03:25 -0400

    Add flask-mysqldb as an extension, set up IDE for DB integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,27 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 from app import configs
 
-def create_app(config="Config"):
+# Instantiate extensions
+db = MySQL()
+
+
+def create_app(config_class="Config"):
+    """
+    Factory method to create an app instance.
+
+    :param config_class: the name of the Config class to import
+    :type config_class: configs.Config
+    :return: a Flask app instance
+    :rtype: Flask
+    """
     app = Flask(__name__)
-    app.config.from_object(configs.__name__ + "." + config)
+
+    # Import the specified Config class from configs.py
+    app.config.from_object(configs.__name__ + "." + config_class)
+
+    # Initialize extensions
+    db.init_app(app)
 
     return app
