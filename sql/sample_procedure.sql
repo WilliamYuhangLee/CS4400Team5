2ee42739959e5bd9ba277ee998ec9c18cb8a2ba9
Lechen Dai <46640649+LechenDai43@users.noreply.github.com>
2019-04-13 22:47:02 -0400

    Write a sample procedure.

---
+++ sql/sample_procedure.sql
@@ -0,0 +1,26 @@
+DELIMITER $$
+CREATE PROCEDURE screen_one_login(in email_address varchar(100), in pass_word varchar(100))
+BEGIN
+	DECLARE username varchar(50);
+	SET @result = " ";
+	SET username = (SELECT UserName FROM Email WHERE EmailAddress LIKE email_address LIMIT 1);
+	IF (SELECT username IS NULL) THEN 
+		SET @result = "Email address does not exist.";
+	ELSE
+		IF (SELECT Password FROM Users WHERE UserName LIKE username LIMIT 1) = pass_word THEN
+			SET @result = "Logging in successfully.";
+		ELSE
+			SET @result = "Password is wrong.";
+		END IF;
+	END IF;
+	SELECT @result;
+END $$
+
+-- Correct way to call it
+
+-- SET @a = "/*your email address*/";
+-- SET @b = "/*your password after hashing*/";
+-- CALL screen_one_login(@a, @b);
+
+-- Then it will return the information about login
+		
