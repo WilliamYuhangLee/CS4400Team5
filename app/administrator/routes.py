146360c0f52d2cd1bf714b25e901a212399e29a4
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-04-21 02:38:48 -0400

    Implement administrator's manage_site view functions and ajax handlers

--- app/administrator/routes.py
+++ app/administrator/routes.py
@@ -42,11 +42,40 @@ def manage_user_send_data():
     return jsonify({"result": True, "message": "Successfully updated the user's status."})
 
 
-@bp.route("/manage-site")
+@bp.route("/manage_site")
 @login_required
 def manage_site():
-    return "Not implemented yet!"  # TODO: implement this method
+    result, error = db_procedure("filter_site_adm", ("", "", 0))
+    if error:
+        raise DatabaseError("An error occurred when getting all sites: " + error)
+    sites = []
+    for row in result:
+        sites.append({
+            "site_name": row[0],
+            "manager_name": row[1],
+            "open_every_date": row[2]
+        })
+    return render_template("administrator-manage-site.html", title="Manage Sites", sites=sites)
 
+
+@bp.route("/manage_site/_send_data", methods=["DELETE"])
+@login_required
+def manage_site_send_data():
+    site_name = request.args.get("site_name", type=str)
+    result, error = db_procedure("delete_site", (site_name,))
+    if error:
+        return jsonify({"result": False, "message": "An error occurred preventing deletion of the site: " + error})
+    result, error = db_procedure("filter_site_adm", ("", "", 0))
+    if error:
+        raise DatabaseError("An error occurred when getting all sites: " + error)
+    sites = []
+    for row in result:
+        sites.append({
+            "site_name": row[0],
+            "manager_name": row[1],
+            "open_every_date": row[2]
+        })
+    return jsonify({"result": True, "message": "Successfully deleted site.", "sites": sites})
 
 @bp.route("/edit-site")
 @login_required
