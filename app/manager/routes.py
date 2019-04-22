3ea1daca2b568cc4d430d566fc683a03fe031574
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-04-21 23:48:22 -0400

    Implement manager's edit_event views and forms

--- app/manager/routes.py
+++ app/manager/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request, jsonify, json
+from flask import render_template, request, jsonify, json, session, flash, redirect, url_for
 from flask_login import login_required, current_user
 from . import bp
+from .forms import EditEventForm
 from app.util import validate_date, db_procedure, DatabaseError
 
 
@@ -47,7 +48,68 @@ def manage_event_send_data():
 @bp.route("/edit-event")
 @login_required
 def edit_event():
-    return "Not implemented yet!"  # TODO: implement this method
+    form = EditEventForm()
+    if form.validate_on_submit():
+        new_staffs = form.staff_assigned.data
+        old_staffs = session[current_user.username]["old_staffs"]
+        site_name = session[current_user.username]["site_name"]
+        all_staffs = session[current_user.username]["all_staffs"]
+        for staff in new_staffs:
+            if staff not in old_staffs:
+                result, error = db_procedure("assign_staff", (site_name, form.name.data, form.start_date.data, all_staffs[staff]))
+                if error:
+                    raise DatabaseError("An error occurred when assigning staff: " + error)
+        for staff in old_staffs:
+            if staff not in new_staffs:
+                result, error = db_procedure("remove_staff", (site_name, form.name.data, form.start_date.data, all_staffs[staff]))
+                if error:
+                    raise DatabaseError("An error occurred when removing staff: " + error)
+        result, error = db_procedure("edit_event",
+                                    (site_name, form.name.data, form.start_date.data, form.description.data))
+        if error:
+            raise DatabaseError("An error occurred when editing description for event: " + error)
+        flash(message="You have successfully edited the event!", category="success")
+        return redirect(url_for(".manage_event"))
+    event_name = request.args.get("event_name")
+    site_name = request.args.get("site_name")
+    start_date = request.args.get("start_date")
+    result, error = db_procedure("query_event_by_pk", (site_name, event_name, start_date))
+    if error:
+        raise DatabaseError("An error occurred when getting event detail: " + error)
+    end_date, min_staff_req, capacity, description, duration, price = result[0]
+    result, error = db_procedure("query_staff_by_event", (site_name, event_name, start_date))
+    if error:
+        raise DatabaseError("An error occurred when querying staff for an event: " + error)
+    old_staffs = {}
+    for staff in result:
+        old_staffs[staff[1] + staff[2]] = staff[0]
+    session[current_user.username]["old_staffs"] = old_staffs
+    session[current_user.username]["site_name"] = site_name
+    all_staffs = old_staffs.copy()
+    # TODO: query all avaiable staffs
+    session[current_user.username]["all_staffs"] = all_staffs
+    form.staff_assigned.choices = [(name, name) for name in all_staffs.keys()]
+    form.name.data = event_name
+    form.price.data = price
+    form.start_date.data = start_date
+    form.end_date.data = end_date
+    form.minimum_staff_required.data = min_staff_req
+    form.capacity.data = capacity
+    form.staff_assigned.data = old_staffs.keys()
+    form.description.data = description
+    result, error = db_procedure("filter_daily_event", (site_name, "0000-00-00"))
+    if error:
+        DatabaseError("An error occurred when getting events for site: " + error)
+    days = []
+    for row in result:
+        days.append({
+            "event_name": row[0],
+            "site_name": row[1],
+            "start_date": row[2],
+            "daily_visit": row[3],
+            "daily_revenue": row[4],
+        })
+    return render_template("manager-edit-event.html", title="Edit Event", form=form, days=json.dumps(days))
 
 
 @bp.route("/create-event")
