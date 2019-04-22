bf4576235ced6f6a0fa90d698ace86d3f28bb495
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-04-21 21:44:11 -0400

    Implement visitor's explore_event views and forms (untested)

---
+++ app/visitor/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, DateField, SubmitField
+from wtforms.validators import InputRequired
+
+
+class EventDetailForm(FlaskForm):
+    event = StringField(label="Event", render_kw={"readonly": True})
+    site = StringField(label="Site", render_kw={"readonly": True})
+    start_date = StringField(label="Start Date", render_kw={"readonly": True})
+    end_date = StringField(label="End Date", render_kw={"readonly": True})
+    ticket_price = IntegerField(label="Capacity", render_kw={"readonly": True})
+    tickets_remaining = IntegerField(label="Tickets Remaining", render_kw={"readonly": True})
+    description = StringField(label="Description", render_kw={"readonly": True})
+    visit_date = DateField(label="Visit Date", validators=[InputRequired()])
+    submit = SubmitField(label="Log Visit")
+
+    def validate_on_submit(self):
+        pass # TODO: implement validation logic
--- app/visitor/routes.py
+++ app/visitor/routes.py
@@ -1,17 +1,64 @@
-from flask_login import login_required
+from flask import render_template, json, request, flash, redirect, url_for
+from flask_login import login_required, current_user
 from . import bp
+from .forms import EventDetailForm
+from app.util import db_procedure, DatabaseError, validate_date
 
 
 @bp.route("/explore-event")
 @login_required
 def explore_event():
-    return "Not implemented yet!"  # TODO: implement this method
+    args = (current_user.username,) + ("",) * 3 + ("0000-00-00",) * 2 + (0,) * 4 + (1,) * 2
+    result, error = db_procedure("filter_event_vis", args)
+    if error:
+        raise DatabaseError("An error occurred when getting all events for a visitor:" + error)
+    events = []
+    for row in result:
+        events.append({
+            "event_name": row[0],
+            "site_name": row[1],
+            "ticket_price": row[2],
+            "ticket_remaining": row[3],
+            "total_visits": row[4],
+            "my_visits": row[5],
+        })
+    return render_template("visitor-explore-event.html", title="Explore Event", events=json.dumps(events))
 
 
 @bp.route("/event-detail")
 @login_required
 def event_detail():
-    return "Not implemented yet!"  # TODO: implement this method
+    form = EventDetailForm()
+    if form.validate_on_submit():
+        date = form.visit_date.data
+        args = (form.site.data, form.event.data, form.start_date.data, current_user.username, date)
+        result, error = db_procedure("log_event", args)
+        if error:
+            raise DatabaseError("An error occurred when logging event for visitor: " + error)
+        flash(message="You have successfully logged your visit!", category="success")
+        return redirect(url_for(".explore_event"))
+    site_name = request.args.get("site_name", type=str)
+    event_name = request.args.get("event_name", type=str)
+    start_date = request.args.get("start_date", type=str)
+    if not validate_date(start_date):
+        raise ValueError("Start date format incorrect!")
+    result, error = db_procedure("query_event_by_pk", (site_name, event_name, start_date))
+    if error:
+        raise DatabaseError("An error occurred when getting event detail for visitor: " + error)
+    end_date, _, _, description, _, price = result[0]
+    args = (current_user.username, event_name, "", site_name, start_date, "0000-00-00") + (0,) * 4 + (1,) * 2
+    result, error = db_procedure("filter_event_vis", args)
+    if error:
+        raise DatabaseError("An error occurred when querying event for visitor:" + error)
+    tickets_remaining = result[3]
+    form.event.data = event_name
+    form.site.data = site_name
+    form.start_date.data = start_date
+    form.end_date.data = end_date
+    form.ticket_price.data = price
+    form.tickets_remaining.data = tickets_remaining
+    form.description.data = description
+    return render_template("visitor-event-detail.html", title="Event Detail", form=form)
 
 
 @bp.route("/explore-site")
